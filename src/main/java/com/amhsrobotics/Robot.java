bb29d5ce30cb144a610eaf1dc8811a1a9ff28ccc
Owen Leather <owenleather22@mittymonarch.com>
2019-10-06 21:31:22 -0700

    Initial commit

---
+++ src/main/java/com/amhsrobotics/Robot.java
@@ -0,0 +1,64 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package com.amhsrobotics;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.amhsrobotics.commands.ExampleCommand;
+import com.amhsrobotics.subsystems.ExampleSubsystem;
+
+public class Robot extends TimedRobot {
+  public static OI m_oi;
+
+  @Override
+  public void robotInit() {
+    m_oi = new OI();
+  }
+
+  @Override
+  public void robotPeriodic() {
+  }
+
+  @Override
+  public void disabledInit() {
+  }
+
+  @Override
+  public void disabledPeriodic() {
+    Scheduler.getInstance().run();
+  }
+
+
+  @Override
+  public void autonomousInit() {
+
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+    Scheduler.getInstance().run();
+  }
+
+  @Override
+  public void teleopInit() {
+
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    Scheduler.getInstance().run();
+  }
+
+
+  @Override
+  public void testPeriodic() {
+  }
+}
