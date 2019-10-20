a0f440df9d2da92139fa82642126ec09811c1f59
Owen Leather <owenleather22@mittymonarch.com>
2019-10-20 14:54:11 -0700

    synthesis simulator testing

--- src/main/java/com/amhsrobotics/Robot.java
+++ src/main/java/com/amhsrobotics/Robot.java
@@ -8,16 +8,31 @@
 package com.amhsrobotics;
 
 import com.amhsrobotics.commands.TankDrive;
+import com.amhsrobotics.constants.DriveConstants;
 import com.amhsrobotics.subsystems.DriveTrain;
-import edu.wpi.first.wpilibj.TimedRobot;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.command.Scheduler;
+
+import java.awt.geom.Point2D;
 
 public class Robot extends TimedRobot {
 
+  public Robot(){
+    super(0.06);
+  }
+
+  Talon talonLeft = new Talon(0);
+  Talon talonRight = new Talon(1);
+  XboxController controller = new XboxController(0);
+
+
   @Override
   public void robotInit() {
-    DriveTrain.getInstance();
-    OI.getInstance();
+    System.out.println("Init");
+    talonLeft.setInverted(false);
+    talonRight.setInverted(true);
   }
 
   @Override
@@ -27,6 +42,8 @@ public class Robot extends TimedRobot {
 
   @Override
   public void disabledInit() {
+    talonLeft.set(0);
+    talonRight.set(0);
   }
 
   @Override
@@ -47,16 +64,69 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
-    new TankDrive().start();
-  }
 
+
+  }
+  double driveDeadzone = 0.01;
+  double turnDeadzone = 0.05;
+  double maxSpeed;
   @Override
   public void teleopPeriodic() {
+    //System.out.println(controller.getTriggerAxis(GenericHID.Hand.kRight) );
 
+    double t = Math.sin(controller.getX(GenericHID.Hand.kLeft)) /2;
+    double d = -((controller.getX()+1) /8) + ((controller.getTriggerAxis(GenericHID.Hand.kRight)+1)/8);
+    d = d * (1-Math.abs((t/2)));
+
+    double r = Math.pow(cot(t*(Math.PI/2)),2) * Math.signum(t) * -Math.signum(d);
+
+    talonLeft.set(leftSpeedFromRadius(r,d));
+    talonRight.set(rightSpeedFromRadius(r,d));
+
+    if(Math.abs(d) > driveDeadzone && Math.abs(t) < turnDeadzone){
+      talonLeft.set(d);
+      talonRight.set(d);
+    }
+
+    if(Math.abs(d) < driveDeadzone && Math.abs(t) < turnDeadzone){
+      talonLeft.stopMotor();
+      talonRight.stopMotor();
+    }
+
+    if(Math.abs(d) < driveDeadzone&& Math.abs(t) > turnDeadzone){
+      talonLeft.set(t);
+      talonRight.set(-t);
+    }
+
+
+
+
+  }
+
+  double trackWidth = 21.2;
+
+  public double leftSpeedFromRadius(double r, double d) {
+    double baseVelocity = d;
+
+    double angularVelocity = baseVelocity / r;
+
+    return angularVelocity * (r - (trackWidth / 2));
+  }
+
+  public double rightSpeedFromRadius(double r, double d) {
+    double baseVelocity = d;
+
+    double angularVelocity = baseVelocity / r;
+
+    return angularVelocity * (r + (trackWidth / 2));
   }
 
 
   @Override
   public void testPeriodic() {
+  }
+
+  private double cot(double x){
+    return (Math.toRadians(Math.cos(x))/Math.toRadians(Math.sin(x)));
   }
 }
